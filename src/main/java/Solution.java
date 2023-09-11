c06afa2da293331ddc1f8911dafc6a5aa884ab05
alek <wait82985@gmail.com>
2023-09-11 17:42:28 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,28 @@
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        return null;
+        Map<Integer, List<List<Integer>>> map = new HashMap<>();
+        for (int i = 0; i < groupSizes.length; i++) {
+            int size = groupSizes[i];
+            if (!map.containsKey(size)) {
+                map.put(size, new ArrayList<>());
+                map.get(size).add(new ArrayList<>());
+            }
+            List<List<Integer>> currentListOfLists = map.get(size);
+            List<Integer> currentList = currentListOfLists.get(currentListOfLists.size() - 1);
+            if (currentList.size() < size) {
+                currentList.add(i);
+            } else {
+                currentListOfLists.add(new ArrayList<>());
+                currentList = currentListOfLists.get(currentListOfLists.size() - 1);
+                currentList.add(i);
+
+            }
+
+        }
+        return map.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
+
     }
 }
